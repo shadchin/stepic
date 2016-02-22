6222c624ef48cc48429dc76172d9b35ab33fd30d
Alexandr Shadchin <alexandr.shadchin@gmail.com>
2016-02-22 20:37:49 +0500

    next

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 
 def wsgi_application(environ, start_response):
-    print('start')
-    body = 'test'
+    body = environ['QueryString']
     status = '200 OK'
     headers = [('Content-Type', 'text/plain'),
         ('Content-Length', str(len(body)))]
