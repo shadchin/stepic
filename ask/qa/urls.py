b4010dfc27f2d87e53f9354c09ce3560f92a4a7b
Alexandr Shadchin <alexandr.shadchin@gmail.com>
2016-03-03 15:40:19 +0500

    next

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^login/$', test),
     url(r'^signup/$', test),
     url(r'^question/(?P<id>[^/]+)/$', test),
-    url(r'^ask/$', test),
+    url(r'^ask', test),
     url(r'^popular/$', test),
     url(r'^new/$', test),
 ]
