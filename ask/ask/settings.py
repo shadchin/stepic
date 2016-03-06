07387d8f3b6d98f868c93fc94df0b8391c1ee1c8
Alexandr Shadchin <alexandr.shadchin@gmail.com>
2016-03-06 13:00:03 +0500

    fix

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root'
+        'USER': 'root',
         'NAME': 'ask',
     }
 }
