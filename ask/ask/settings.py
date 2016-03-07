4462ac2ed955d7f40ec6ca3387c1367b7f00eb51
Alexandr Shadchin <alexandr.shadchin@gmail.com>
2016-03-07 21:46:40 +0500

    auth

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'NAME': 'ask',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.mysql',
+#        'USER': 'root',
+#        'NAME': 'ask',
     }
 }
 
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'auth.User'
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,13 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+
+from ask import settings
 
 # Create your models here.
 class Likes(models.Model):
     added_at = models.DateTimeField()
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
     class META:
         db_table = 'likes'
 
@@ -17,7 +18,7 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField()
     rating = models.IntegerField(null=True)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
     likes = models.ForeignKey(Likes, null=True, on_delete=models.SET_NULL)
 
     def get_url(self):
@@ -30,7 +31,7 @@ class Question(models.Model):
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField()
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     class META:
         db_table = 'answer'
