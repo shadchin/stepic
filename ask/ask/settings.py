1d05153ddd74b592a6c6e7cae96eeafa209a4197
Alexandr Shadchin <alexandr.shadchin@gmail.com>
2016-03-07 21:56:58 +0500

    fix author

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db1.sqlite3'),
 #        'ENGINE': 'django.db.backends.mysql',
 #        'USER': 'root',
 #        'NAME': 'ask',
