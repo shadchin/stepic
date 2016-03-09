9ca0ad0c24c206d62df43d4a59ee5955c6bdb33f
Alexandr Shadchin <alexandr.shadchin@gmail.com>
2016-03-09 22:58:31 +0500

    auth

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'root',
-        'NAME': 'ask',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.mysql',
+#        'USER': 'root',
+#        'NAME': 'ask',
     }
 }
 
